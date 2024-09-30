4ba9cba6ba6d5bd1b8bc571de87d57e0c23c940b
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2024-09-30 15:41:16 +0300

    add scroll for editor

--- _frontend/entrypoints/application.js
+++ _frontend/entrypoints/application.js
@@ -1,11 +1,14 @@
 import Alpine from "alpinejs";
 import { registerModules } from "../utils/register_modules";
+import Autosize from "@marcreichel/alpine-autosize";
 
 registerModules(
   import.meta.glob("../components/**/index.js", {
     eager: true,
   }),
 );
+
+Alpine.plugin(Autosize);
 
 window.Alpine = Alpine;
 
