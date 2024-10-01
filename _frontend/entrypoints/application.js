b8922cda9268a388716409cf6d0b359e7ad5219d
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2024-10-02 00:07:30 +0300

    add popover polyfill

--- _frontend/entrypoints/application.js
+++ _frontend/entrypoints/application.js
@@ -1,6 +1,7 @@
 import Alpine from "alpinejs";
 import { registerModules } from "../utils/register_modules";
 import Autosize from "@marcreichel/alpine-autosize";
+import "../vendors/css-anchor-positioning";
 
 registerModules(
   import.meta.glob("../components/**/index.js", {
---
+++ _frontend/vendors/css-anchor-positioning.js
@@ -0,0 +1,3 @@
+if (!("anchorName" in document.documentElement.style)) {
+  import("https://unpkg.com/@oddbird/css-anchor-positioning");
+}
